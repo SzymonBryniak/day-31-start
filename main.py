a05aef87b7a48e48b76a0e25b18025bd06155333
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-09-13 19:05:09 +0100

    Words fetched from csv now display on the card.

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ window.config(pady=20, padx=20, bg=BACKGROUND_COLOR)
 window.geometry('496x396')
 
 
+def update_card():
+    with open('./data/french_words.csv') as words:
+        word = words.readline()
+        split = word.split(',')[1]
+        print(split)
+        canvas.create_text(220, 100, text=split, fill="black", font=('Helvetica 15 bold'))
+
+
+def red_button():
+    update_card()
+
+
+def green_button():
+    pass
+
+
 img_front = (Image.open("./images/card_front.png"))
 img_button_left = (Image.open("./images/wrong.png"))
 img_button_right = (Image.open("./images/right.png"))
@@ -26,7 +42,7 @@ new_button_right = ImageTk.PhotoImage(resized_button_right)
 
 canvas = tkinter.Canvas(window, width=450, height=305, bg=BACKGROUND_COLOR)
 canvas.create_image(220, 150, image=new_image)
-canvas.create_text(220, 150, text="HELLO WORLD", fill="black", font=('Helvetica 15 bold'))
+text1 = canvas.create_text(220, 150, text="HELLO WORLD", fill="black", font=('Helvetica 15 bold'))
 canvas.grid(column=0, row=0, columnspan=1)
 
 button_left = Button(image=new_button_left, highlightthickness=0)
@@ -34,7 +50,7 @@ button_left.config(height=50, width=50)
 button_left.place(x=97, y=305)
 # button_left.grid(column=0, row=1, sticky='w')
 
-button_right = Button(image=new_button_right, highlightthickness=0)
+button_right = Button(image=new_button_right, highlightthickness=0, command=red_button)
 button_right.place(x=275, y=305)
 # button_right.grid(column=1, row=1)
 tkinter.mainloop()
