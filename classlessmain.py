2640b932e2a4eac06833b3f604958cbaef6ecb50
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-09-15 19:48:19 +0100

    My solution to the challenge in day 31

--- classlessmain.py
+++ classlessmain.py
@@ -1,6 +1,7 @@
 import tkinter
 from tkinter import *
 from PIL import Image, ImageTk
+import random
 import time
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -27,10 +28,12 @@ new_button_left = ImageTk.PhotoImage(resized_button_left)
 resized_button_right = img_button_right.resize((50, 50))
 new_button_right = ImageTk.PhotoImage(resized_button_right)
 
-canvas = tkinter.Canvas(window, width=450, height=305, bg=BACKGROUND_COLOR)
+canvas = tkinter.Canvas(window, width=450, height=305, bg=BACKGROUND_COLOR, highlightthickness=0)
 
 counter_english = [0]
 counter_french = [0]
+random_index = 0
+exclude = []
 # todo: take out words guessed correctly
 
 
@@ -40,42 +43,47 @@ def start_card(del_text=0):
     canvas.grid(column=0, row=0, columnspan=1)
 
 
-def front_card():
-    global counter_english
-    canvas.create_image(220, 150, image=front_image)
-    with open('./data/french_words.csv') as words:
-        title_word = words.readline()
-        title = title_word.split(',')[1]
-
-        word = words.readlines()
-        print(word)
-
-        canvas.create_text(220, 100, text=title, fill="black", font=('Helvetica 15 bold'))
-        canvas.create_text(220, 130, text=word[counter_english[-1]].split(',')[1].strip('\n'), fill="black", font=('Helvetica 15 bold'))
-        counter_english.append(len(counter_english)+1)
-
-
-def back_card():
-    global counter_french
+def english_card(excl):
+    global counter_english, random_index
     canvas.create_image(220, 150, image=back_image)
     with open('./data/french_words.csv') as words:
         title_word = words.readline()
-        title = title_word.split(',')[0]
-
+        title = title_word.split(',')[1]
         word = words.readlines()
         print(word)
+    random_index = random.choice([i for i in range(len(word)) if i not in exclude])
+    canvas.create_text(220, 100, text=title, fill="black", font=('Helvetica 15 bold'))
+    canvas.create_text(220, 130, text=word[random_index].split(',')[1].strip('\n'), fill="black", font=('Helvetica 15 bold'))
+    counter_english.append(len(counter_english)+1)
 
-        canvas.create_text(220, 100, text=title, fill="black", font=('Helvetica 15 bold'))
-        canvas.create_text(220, 130, text=word[counter_french[-1]].split(',')[0].strip('\n'), fill="black", font=('Helvetica 15 bold'))
-        counter_french.append(len(counter_french)+1)
+    window.after(3000, lambda: french_card(excl))
 
 
-button_left = Button(image=new_button_left, highlightthickness=0, command=back_card)
+def french_card(excl):
+    global counter_french, random_index, exclude
+    if excl == 1:
+        exclude.append(random_index)
+        print(exclude)
+    canvas.create_image(220, 150, image=front_image)
+    with open('./data/french_words.csv') as words:
+        title_word = words.readline()
+        title = title_word.split(',')[0]
+        word = words.readlines()
+
+    canvas.create_text(220, 100, text=title, fill="black", font=('Helvetica 15 bold'))
+    canvas.create_text(220, 130, text=word[random_index].split(',')[0].strip('\n'), fill="black", font=('Helvetica 15 bold'))
+    counter_french.append(len(counter_french)+1)
+    print(excl)
+
+    group = random.choice([i for i in range(200, 1000) if i not in exclude])
+
+
+button_left = Button(image=new_button_left, highlightthickness=0, command=lambda: english_card(0), borderwidth=0)
 button_left.config(height=50, width=50)
 button_left.place(x=97, y=305)
 # button_left.grid(column=0, row=1, sticky='w')
 
-button_right = Button(image=new_button_right, highlightthickness=0, command=front_card)
+button_right = Button(image=new_button_right, highlightthickness=0, command=lambda: english_card(1), borderwidth=0)
 button_right.place(x=275, y=305)
 # button_right.grid(column=1, row=1)
 
