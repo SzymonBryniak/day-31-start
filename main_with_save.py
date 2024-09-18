149c80b631c838030c205d2abcb7579e0da8d846
Szymon Bryniak <szymonbryniakproject@gmail.com>
2024-09-18 20:56:21 +0100

    Saving to file with pandas is now working

--- main_with_save.py
+++ main_with_save.py
@@ -37,11 +37,17 @@ counter_french = [0]
 random_index = 0
 exclude = []
 to_learn = {}
-# todo: save not guessed words to csv
+load_words = './data/french_words.csv'
+# to_learn.csv can be loaded by replacing the file path
+# in the "load_words" variable with the file path of the to_lear.csv.
 
-to_learn_df = pandas.DataFrame({'French': {}, 'English': {}})
 
-print(to_learn_df)
+
+try:
+    to_learn_df = pandas.read_csv('./data/to_learn.csv', index_col=0)
+    # to_learn_df = to_learn_df.loc[:, ~to_learn_df.columns.str.contains('^Unnamed')]
+except FileNotFoundError:
+    to_learn_df = pandas.DataFrame({'French': {}, 'English': {}, 'Index': {}})
 
 
 def start_card(del_text=0):
@@ -53,64 +59,48 @@ def start_card(del_text=0):
 def english_card(excl):
     global counter_english, random_index
     canvas.create_image(220, 150, image=back_image)
-    with open('./data/french_words.csv') as words:
+    with open(load_words) as words:
         title_word = words.readline()
         title = title_word.split(',')[1]
         word = words.readlines()
-        print(word)
     random_index = random.choice([i for i in range(len(word)) if i not in to_learn_df])
     random_word_to_exclude = word[random_index]
     random_word = random_word_to_exclude.split(',')[1].strip('\n')
 
-    # print(random_index)
-
     canvas.create_text(220, 100, text=title, fill="black", font=('Helvetica 15 bold'))
     canvas.create_text(220, 130, text=random_word, fill="black", font=('Helvetica 15 bold'))
-    counter_english.append(len(counter_english)+1)
 
     french_word_to_learn = random_word_to_exclude.strip('\n').split(',')[0]
     english_word_to_learn = random_word_to_exclude.strip('\n').split(',')[1]
     to_learn_df.loc[len(exclude), 'French'] = french_word_to_learn
     to_learn_df.loc[len(exclude), 'English'] = english_word_to_learn
-    print(to_learn_df)
+    to_learn_df.loc[len(exclude), 'Index'] = int(random_index)
 
     if excl == 1:
         to_learn_df.drop(index=to_learn_df.index[-1], axis=0, inplace=True)
-    to_learn_df.to_csv('./data/excluded.csv')
+    to_learn_df.to_csv('./data/to_learn.csv', index=False)  # option index=False will save the file without the index column.
     window.after(3000, lambda: french_card())
 
 
 def french_card():
     global counter_french, random_index, exclude, to_learn_df
     canvas.create_image(220, 150, image=front_image)
-    with open('./data/french_words.csv') as words:
+    with open(load_words) as words:
         title_word = words.readline()
         title = title_word.split(',')[0]
         word = words.readlines()
         word_object = word[random_index].split(',')[0].strip('\n')
-        print(word_object)
-
         exclude.append(random_index)
-        # print(exclude)
-
-        # to_learn_df.update(word_to_learn)
     canvas.create_text(220, 100, text=title, fill="black", font=('Helvetica 15 bold'))
     canvas.create_text(220, 130, text=word_object, fill="black", font=('Helvetica 15 bold'))
-    counter_french.append(len(counter_french)+1)
-    # print(excl)
-
-    group = random.choice([i for i in range(200, 1000) if i not in exclude])
 
 
 button_left = Button(image=new_button_left, highlightthickness=0, command=lambda: english_card(0), borderwidth=0)
 button_left.config(height=50, width=50)
 button_left.place(x=97, y=305)
-# button_left.grid(column=0, row=1, sticky='w')
-
 button_right = Button(image=new_button_right, highlightthickness=0, command=lambda: english_card(1), borderwidth=0)
 button_right.place(x=275, y=305)
-# button_right.grid(column=1, row=1)
-to_learn_df.to_csv('./data/excluded.csv')
-start_card()
 
+
+start_card()
 tkinter.mainloop()
